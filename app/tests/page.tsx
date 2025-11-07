d85bff2a68575dc1be6698f084db79383de2b80f
SarayuJ3 <jilludumudisarayu@gmail.com>
2025-11-06 19:49:00 -0500

    added testing for approve/reject for items just tested with a singular item in a basic donation request

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -1,10 +1,70 @@
+'use client';
+
+import { acceptRequestItem, deleteRequestItem, getDonationRequest, setDonationRequest } from '@/lib/services/donations';
+import { DonationRequest, DonationItem } from '@/types/donations';
+import { Timestamp } from 'firebase/firestore';
+
 export default function testPage() {
-    console.log( {
-        a: [1, 4, 5],
-        b: 'hello'
-    } )
+    const testRequest: DonationRequest = {
+            id: 'donation',
+            donor: {
+                firstName: '',
+                lastName: '',
+                email: '',
+                phoneNumber: '',
+                address: {
+                    streetAddress: '',
+                    city: '',
+                    state: '',
+                    zipCode: '',
+                }
+        },
+        firstTimeDonor: true,
+        howDidYouHear: '',
+        canDropOff: true,
+        notes: '',
+        date: Timestamp.now(),
+        items: [
+            {
+                item: {
+                    id: 'item',
+                    name: '',
+                    photos: [],
+                    category: '',
+                    notes: '',
+                    quantity: 1,
+                    size: 'Small',
+                    dateAdded: Timestamp.now(),
+                    donorEmail: '',
+                },
+                status: 'Not Reviewed'
+            }
+        ]
+    };
+
+    console.log(testRequest)
+
+    const handleAccept = async () => {
+        await setDonationRequest(testRequest);
+        await acceptRequestItem(testRequest.id, 'item');
+        const updated = await getDonationRequest(testRequest.id);
+        console.log(updated);
+    };
+
+    const handleReject = async () => {
+        await setDonationRequest(testRequest);
+        await deleteRequestItem(testRequest.id, 'item');
+        const updated = await getDonationRequest(testRequest.id);
+        console.log(updated);
+    };
 
     return <>
         <h1>hi</h1>
+        <div>
+        <button onClick={handleAccept}>accept</button>
+        </div>
+        <div>
+        <button onClick={handleReject}>reject</button>
+        </div>
     </>;
 }
