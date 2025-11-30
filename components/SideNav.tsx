fdf0f2d2991c3462f788631514989613957b3cca
Ritika Pokharel <rpokhar@terpmail.umd.edu>
2025-11-30 18:56:50 -0500

    Add button

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,21 +1,14 @@
 "use client";
 
-import Link from "next/link";
-
 export default function SideNavbar() {
-  return (
-    <div className="h-full w-50 p-4 flex flex-col font-family-roboto">
-      <span className="text-primary font-extrabold pb-4">Admin</span>
-      <span className="pb-4 text-sm">Inventory</span>
-      <span className="pb-4 text-sm">Volunteers</span>
-      <Link
-        href="/case-manager"
-        className="pb-4 text-sm hover:underline cursor-pointer"
-      >
-        Case Managers
-      </Link>
 
-      <span className="pb-4 text-sm">User Management</span>
-    </div>
-  );
+    return (
+        <div className="h-full w-50 p-4 flex flex-col font-family-roboto">
+            <span className="text-primary font-extrabold pb-4">Admin</span>
+            <span className="pb-4 text-sm">Inventory</span>
+            <span className="pb-4 text-sm">Volunteers</span>
+            <span className="pb-4 text-sm">Case Managers</span>
+            <span className="pb-4 text-sm">User Management</span>
+        </div>
+    )
 }
