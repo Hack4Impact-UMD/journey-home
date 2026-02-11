162a4d6808493c35ac1bc56bbdb192c5cbf0a525
Joel Chemmanur <joelchem@outlook.com>
2026-02-11 01:45:02 -0500

    Minor nonexistant import fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import localFont from "next/font/local";
 import { AuthProvider } from "../contexts/AuthContext";
 import { Toaster } from "@/components/ui/sonner";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "@/lib/queryClient";
 import { Providers } from "./providers";
 
 const openSans = localFont({
