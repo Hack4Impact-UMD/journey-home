049142dc9f973bdde95361ef65a5fba9ca4108f1
Joel Chemmanur <joelchem@outlook.com>
2026-02-16 08:28:04 -0500

    Added category attributes + query for it and fixed up some test data generation

--- app/donate/steps/Step2AddDonations.tsx
+++ app/donate/steps/Step2AddDonations.tsx
@@ -7,11 +7,11 @@ import FormInput from "../../../components/form/FormInput";
 import FormSelect from "../../../components/form/FormSelect";
 import FormTextarea from "../../../components/form/FormTextarea";
 import Button from "../../../components/form/Button";
-import { useCategories } from "@/lib/services/inventory";
 import { CloseIcon } from "@/components/icons/CloseIcon";
 import { toast } from "sonner";
 import { PlusIcon } from "lucide-react";
 import Image from "next/image";
+import { useCategories } from "@/lib/queries/categories";
 
 
 export default function Step2AddDonations() {
@@ -39,7 +39,7 @@ export default function Step2AddDonations() {
         setCurrentStep(3);
     };
 
-    const categoryOptions = useCategories();
+    const {allCategories: categoryOptions} = useCategories();
 
     const fileInputRefs = useRef<Map<string, HTMLInputElement>>(new Map());
 
---
+++ lib/queries/categories.ts
@@ -0,0 +1,63 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getCategoryAttributes, setCategoryAttributes } from "../services/inventory";
+import { CategoryAttributes } from "@/types/inventory";
+import { toast } from "sonner";
+
+export function useCategories() {
+
+    const queryClient = useQueryClient();
+
+    const query = useQuery({
+        queryKey: ["categories"],
+        queryFn: getCategoryAttributes
+    });
+
+    const setMutation = useMutation({
+        mutationFn: setCategoryAttributes,
+        onMutate: async (attrs: CategoryAttributes[]) => {
+            await queryClient.cancelQueries({queryKey: ["categories"]});
+            const prevData = queryClient.getQueryData<CategoryAttributes[]>(["categories"]);
+
+            queryClient.setQueryData(["users"], attrs);
+            return { prevData }
+        },
+        onError: (error, attrs, context) => {
+            if (context?.prevData) {
+                queryClient.setQueryData(["categories"], context.prevData);
+            }
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["categories"] });
+        }
+    });
+
+    const setCategoriesWithToast = async (attrs: CategoryAttributes[]) => {
+        const promise = setMutation.mutateAsync(attrs);
+        toast.promise(promise, {
+            loading: "Updating categories...",
+            success: "User categories successfully!",
+            error: "Error: Couldn't update categories",
+        });
+        await promise;
+    }
+
+    const fallback = {
+        name: "Other",
+        lowThreshold: 0,
+        highThreshold: 0
+    } as CategoryAttributes;
+
+    return {
+        allAttrs: query.data ?? [fallback,],
+        allCategories: query.data?.map(attr => attr.name) ?? [fallback.name,],
+
+        setCategories: setMutation.mutateAsync,
+        setCategoriesWithToast,
+
+        isLoading: query.isLoading,
+        isError: query.isError,
+        error: query.error,
+        refetch: query.refetch
+    }
+
+}
