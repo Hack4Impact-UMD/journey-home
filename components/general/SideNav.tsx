4b78d8588aea2d9752fd716034652d403a6c754a
Alisha <alishawu2024@gmail.com>
2026-02-28 14:24:06 -0500

    adding in icons

--- components/general/SideNav.tsx
+++ components/general/SideNav.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
+import React from "react";
 import { usePathname } from "next/navigation";
 import { useAuth } from "@/contexts/AuthContext";
 import { UserRole } from "@/types/user";
 import Link from "next/link";
+import {InventoryIcon} from "../icons/InventoryIcon";
+import { ClientRequestIcon } from "../icons/ClientRequestIcon";
 
 export default function SideNavbar() {
     const auth = useAuth();
@@ -14,51 +17,21 @@ export default function SideNavbar() {
                 {auth.state.userData?.role ?? "Loading..."}
             </Link>
 
-            <SideNavbarLinkGroup
+            <SideNavbarLink
+                icon = {InventoryIcon}
                 name="Inventory"
                 path="/inventory"
                 roles={["Admin", "Volunteer"]}
-            >
-                <SideNavbarLink
-                    name="Warehouse"
-                    path="/inventory/warehouse"
-                    roles={["Admin"]}
-                />
-                <SideNavbarLink
-                    name="Donation Requests"
-                    path="/inventory/donation-requests"
-                    roles={["Admin"]}
-                />
-                <SideNavbarLink
-                    name="Reviewed Donations"
-                    path="/inventory/reviewed-donations"
-                    roles={["Admin"]}
-                />
-            </SideNavbarLinkGroup>
+            />
 
-            <SideNavbarLinkGroup
+            <SideNavbarLink
                 name="User Management"
                 path="/user-management"
                 roles={["Admin"]}
-            >
-                <SideNavbarLink
-                    name="All Accounts"
-                    path="/user-management/all-accounts"
-                    roles={["Admin"]}
-                />
-                <SideNavbarLink
-                    name="Account Requests"
-                    path="/user-management/account-requests"
-                    roles={["Admin"]}
-                />
-                <SideNavbarLink
-                    name="Past Donors"
-                    path="/user-management/past-donors"
-                    roles={["Admin"]}
-                />
-            </SideNavbarLinkGroup>
+            />
 
             <SideNavbarLink
+                icon = {ClientRequestIcon}
                 name="Client Requests"
                 path="/client-requests"
                 roles={["Admin", "Case Manager"]}
@@ -87,54 +60,6 @@ export default function SideNavbar() {
                 path="/profile"
                 roles={["Admin", "Case Manager", "Volunteer"]}
             />
-
-
-        </div>
-    );
-}
-
-function SideNavbarLinkGroup({
-    name,
-    path,
-    roles,
-    children,
-}: {
-    name: string;
-    path: string;
-    roles: UserRole[];
-    children: React.ReactNode;
-}) {
-    const [groupOpen, setGroupOpen] = useState<boolean>(false);
-    const pathname = usePathname();
-    const auth = useAuth();
-
-    useEffect(() => {
-        if (pathname?.startsWith(path)) {
-            setGroupOpen(true);
-        }
-    }, [path, pathname]);
-
-    if (
-        !auth.state.userData ||
-        (roles.length > 0 && !roles.includes(auth.state.userData.role))
-    ) {
-        return <></>;
-    }
-
-    return (
-        <div className="flex flex-col">
-            <button
-                onClick={() => setGroupOpen((x) => !x)}
-                className={`text-left pb-4 text-sm flex items-center justify-between ${
-                    pathname?.startsWith(path)
-                        ? "text-primary font-semibold"
-                        : ""
-                }`}
-            >
-                <span>{name}</span>
-                <span>{groupOpen ? "-" : "+"}</span>
-            </button>
-            {groupOpen && <div className="pl-6 flex flex-col">{children}</div>}
         </div>
     );
 }
@@ -143,10 +68,12 @@ function SideNavbarLink({
     name,
     path,
     roles,
+    icon :Icon,
 }: {
     name: string;
     path: string;
     roles: UserRole[];
+    icon?: React.FC;
 }) {
     const pathname = usePathname();
     const auth = useAuth();
@@ -161,10 +88,11 @@ function SideNavbarLink({
     return (
         <Link
             href={path}
-            className={`hover:text-primary pb-3 text-sm ${
+            className={`hover:text-primary pb-3 text-sm flex items-center gap-2 ${
                 pathname?.startsWith(path) ? "text-primary font-semibold" : ""
             }`}
         >
+            {Icon && <Icon />}
             {name}
         </Link>
     );
