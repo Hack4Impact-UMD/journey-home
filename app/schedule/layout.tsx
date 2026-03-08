eaaef66794a2c0ef08aada435ffa1085a928b94e
anya-glide <anya@bivocash.com>
2026-03-08 14:36:48 -0400

    removed top nav bar

--- app/schedule/layout.tsx
+++ app/schedule/layout.tsx
@@ -2,13 +2,12 @@
 
 import { ProtectedRoute } from "@/components/general/ProtectedRoute";
 import SideNavbar from "@/components/general/SideNav";
-import TopNavbar from "@/components/general/TopNav";
 
 export default function ScheduleLayout({ children }: { children: React.ReactNode }) {
     return (
         <ProtectedRoute allow={["Admin", "Case Manager", "Volunteer"]}>
             <div className="h-full w-full flex flex-col font-family-roboto">
-                <TopNavbar />
+                
                 <div className="flex flex-1">
                     <SideNavbar />
                     <div className="flex-1 bg-[#F7F7F7] py-4 px-6 flex flex-col">
