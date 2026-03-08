ce55a181656a726e42e770510cc8d6dab47c16d8
Joel Chemmanur <joelchem@outlook.com>
2026-03-08 06:26:54 -0400

    minor topbar remnants removal

--- app/client-requests/admin/page.tsx
+++ app/client-requests/admin/page.tsx
@@ -46,7 +46,6 @@ export default function ClientRequestsAdminPage() {
     return (
         <ProtectedRoute allow={["Admin"]}>
             <div className="h-full w-full flex flex-col font-family-roboto">
-                <TopNavbar />
                 <div className="flex flex-1">
                     <SideNavbar />
                     <div className="flex-1 bg-[#F7F7F7] py-4 px-6 flex flex-col">
--- app/client-requests/case-manager/page.tsx
+++ app/client-requests/case-manager/page.tsx
@@ -32,7 +32,6 @@ export default function ClientRequestsCaseManagerPage() {
     return (
         <ProtectedRoute allow={["Case Manager"]}>
             <div className="h-full w-full flex flex-col font-family-roboto">
-                <TopNavbar />
                 <div className="flex flex-1">
                     <SideNavbar />
                     <div className="flex-1 bg-[#F7F7F7] py-4 px-6 flex flex-col">
