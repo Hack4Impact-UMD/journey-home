d4e5f5884147a0389e58efa08432ead6c5cc6124
Alisha <alishawu2024@gmail.com>
2026-03-13 00:00:47 -0400

    please work

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-git rm -r --cached .firebase-cacheimport { dirname } from "path";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
