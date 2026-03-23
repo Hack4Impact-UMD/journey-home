c5640d497a23ce34806866bfe24e26127179b128
shreyas-thirumale <shreyas.thirumale25@gmail.com>
2026-03-22 22:37:30 -0400

    remove topnav import

--- app/control-panel/layout.tsx
+++ app/control-panel/layout.tsx
@@ -2,7 +2,6 @@
 
 import { ProtectedRoute } from "@/components/general/ProtectedRoute";
 import SideNavbar from "@/components/general/SideNav";
-import TopNavbar from "@/components/general/TopNav";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
@@ -13,7 +12,6 @@ export default function ControlPanelLayout({ children }: { children: ReactNode }
     return (
         <ProtectedRoute allow={["Admin"]}>
             <div className="h-full w-full flex flex-col font-family-roboto overflow-hidden">
-                <TopNavbar />
                 <div className="flex flex-1 min-h-0">
                     <SideNavbar />
                     <div className="flex-1 min-h-0 bg-[#F7F7F7] py-4 px-6 flex flex-col">
