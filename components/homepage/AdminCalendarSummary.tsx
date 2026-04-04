33e80ebcc84f0b0268f4030526ae8e32911fd448
Alisha <alishawu2024@gmail.com>
2026-04-04 16:56:38 -0400

    very basic layout for homepage

--- components/homepage/AdminCalendarSummary.tsx
+++ components/homepage/AdminCalendarSummary.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { useTimeBlocks } from "@/lib/queries/timeblocks";
+import { TimeBlock } from "@/types/schedule";
+import { AdminCalendarPeople } from "../icons/AdminCalendarPeople";
 
 export default function AdminCalendarSummary(){
     return (
-        <div>
+        <div className = "rounded-2xl">
             
         </div>
     );
--- components/homepage/AdminHomePage.tsx
+++ components/homepage/AdminHomePage.tsx
@@ -1,5 +1,6 @@
 import { ProtectedRoute } from "@/components/general/ProtectedRoute";
 import SideNavbar from "@/components/general/SideNav";
+import AdminCalendarSummary from "./AdminCalendarSummary";
 
 export default function AdminHomePage() {
     return (
@@ -7,10 +8,20 @@ export default function AdminHomePage() {
             <div className="h-full w-full flex flex-col font-family-roboto">
                 <div className="flex flex-1">
                     <SideNavbar />
-                    <div className="flex-1 bg-[#F7F7F7] pt-8 pb-4 px-6 flex flex-col">
-                        <span className="text-2xl text-primary font-extrabold block">
-                            Journeying to the Home Page! (Admin Home Page)
-                        </span>
+                    <div className="flex-1 flex flex-col bg-[#F7F7F7] overflow-hidden">
+                        
+                        <div className = "flex flex-col w-[90%] h-[93%] bg-[#FFFFFF] mx-[2%] mt-[2.5%] rounded-xl">
+                            <h1 className = ""> Welcome __! </h1>
+                            <div className = "flex gap-4 flex-1">
+                                <div className = "flex flex-col gap-4 w-[45%]"> 
+                                    Left Col Placeholder
+                                </div>
+                                <div className = "flex flex-col gap-4 flex-1">
+                                    Right Col Placeholder
+                                    <AdminCalendarSummary/>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
