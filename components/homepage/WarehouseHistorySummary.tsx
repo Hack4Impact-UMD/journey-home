97876ac2e3b166fb5106c8145a068f0219dabbf3
Alisha <alishawu2024@gmail.com>
2026-04-04 17:01:52 -0400

    setting up other tsxs

---
+++ components/homepage/WarehouseHistorySummary.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { InventoryChange } from "@/types/inventory";
+
+export default function WarehouseHistorySummary(){
+    return (
+        <div>
+
+        </div>
+    );
+}
