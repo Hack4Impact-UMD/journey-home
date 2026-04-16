d1bd578e54ea6a35df1a3e76fa268286d2070757
Aarav Verma <aarav.verma2024@gmail.com>
2026-04-15 21:17:07 -0400

    fixed linting issues and removed leftover useless stuff from debug page

--- app/debug/data/page.tsx
+++ app/debug/data/page.tsx
@@ -13,7 +13,7 @@ import { UserData } from "@/types/user";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { TimeBlock } from "@/types/schedule";
 import { collection, getDocs } from "firebase/firestore";
-import { useWarehouseHistory } from "@/lib/queries/warehouse-history";
+
 import { toast, Toaster } from "sonner";
 
 
@@ -1341,8 +1341,6 @@ const CLIENT_NOTES = [
 ];
 export default function page() {
     const btnClass = "border border-gray-400 rounded px-6 py-2 hover:bg-gray-100 text-sm w-64";
-    const { changes, isLoading } = useWarehouseHistory();
-    const sorted = [...changes].sort((a, b) => a.timestamp.seconds - b.timestamp.seconds);
 
     return (
         <div className="flex flex-col gap-8 m-10 items-center">
