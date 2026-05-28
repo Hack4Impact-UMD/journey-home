1f368949cbbb2ea09a281253363ba15ff9e7d1c1
Joel Chemmanur <joelchem@outlook.com>
2026-05-28 05:49:04 -0400

    more waiver backend

---
+++ lib/queries/waivers.ts
@@ -0,0 +1,38 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { fetchAllWaivers, addWaiver } from "../services/waivers";
+import { toast } from "sonner";
+
+export function useWaivers() {
+    const queryClient = useQueryClient();
+
+    const query = useQuery({
+        queryKey: ["waivers"],
+        queryFn: fetchAllWaivers,
+    });
+
+    const addMutation = useMutation({
+        mutationFn: addWaiver,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["waivers"] });
+        },
+    });
+
+    const uploadWaiver = async (file: File) => {
+        const promise = addMutation.mutateAsync(file);
+        toast.promise(promise, {
+            loading: "Updating waiver...",
+            success: "Waiver updated successfully.",
+            error: "Failed to udpate waiver.",
+        });
+        await promise;
+    };
+
+    return {
+        waivers: query.data ?? [],
+        activeWaiver: query.data?.find((w) => w.end === null) ?? null,
+        uploadWaiver,
+        isMutating: addMutation.isPending,
+        isLoading: query.isLoading,
+        isError: query.isError,
+    };
+}
